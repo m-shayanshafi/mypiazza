bdf93a09e1d316bff18fdd61aae42d0fadfac034
Muhammad Shayan <mshayanshafi@gmail.com>
2016-03-07 14:25:54 +0000

    login page implemented

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+# Bundle edge Rails instead: gem 'rails', github: 'rails/
+gem 'devise'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  devise_scope :user do
+    root :to => 'devise/sessions#new'
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'courses#index'
-
+  # root_to: "courses#in"
+  # root 'courses#index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
